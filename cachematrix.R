44c10578f5d64c42f80fdd1005144e0796ba0867
rudini1 <32279941+rudini1@users.noreply.github.com>
2017-10-13 22:41:38 +0200

    cachematrix.R written for assignment2
    Programming assignment 2
    Functions makeCacheMatrix, cacheSolve written
    Lexical Scope
    Test_Cachematrix.R to test functionality of assignment functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache its inverse.
+## 
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        matrixinverse <- NULL
+        set <- function(y) {
+                x <<- y
+                matrixinverse <<- NULL
+        }
+        get <- function()
+                x
+        setInverseMatrix <-
+                function(Inv)
+                        matrixinverse <<- Inv
+        getInverseMatrix <- function()
+                matrixinverse
+        list(
+                set = set,
+                get = get,
+                setInverseMatrix = setInverseMatrix,
+                getInverseMatrix = getInverseMatrix
+        )
 }
 
+# makeVector <- function(x = numeric()) {
+#         m <- NULL
+#         set <- function(y) {
+#                 x <<- y
+#                 m <<- NULL
+#         }
+#         get <- function() x
+#         setmean <- function(mean) m <<- mean
+#         getmean <- function() m
+#         list(set = set, get = get,
+#              setmean = setmean,
+#              getmean = getmean)
+# }
 
-## Write a short comment describing this function
+
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+                matrixinverse <- x$getInverseMatrix()
+                if(!is.null(matrixinverse)) {
+                        message("getting cached data")
+                        return(matrixinverse)
+                }
+                data <- x$get()
+                matrixinverse <- solve(data, ...)
+                x$setInverseMatrix(matrixinverse)
+                matrixinverse
 }
+
+
+# cachemean <- function(x, ...) {
+#         m <- x$getmean()
+#         if(!is.null(m)) {
+#                 message("getting cached data")
+#                 return(m)
+#         }
+#         data <- x$get()
+#         m <- mean(data, ...)
+#         x$setmean(m)
+#         m
+# }
---
+++ test_cachematrix.R
@@ -0,0 +1,3 @@
+cached <- makeCacheMatrix(matrix(c(2,3,2,4,6,5,3,5,7), 3, 3))
+cacheSolve(cached)
+
